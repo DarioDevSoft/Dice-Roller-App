259cec3f70a6d2cf9509499c8ab127c107f2df2c
Eder Hernández <dariotecsoft90@gmail.com>
2025-04-11 21:59:18 -0500

    The composable element button is added to the random function

--- app/src/main/java/com/example/diceroller/MainActivity.kt
+++ app/src/main/java/com/example/diceroller/MainActivity.kt
@@ -35,6 +35,7 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result = 1
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
@@ -44,7 +45,7 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
             contentDescription = "1"
         )
         Spacer(modifier = Modifier.height(16.dp))
-        Button(onClick = {}) {
+        Button(onClick = { result = (1..6).random()}) {
             Text( stringResource(R.string.roll))
         }
     }
